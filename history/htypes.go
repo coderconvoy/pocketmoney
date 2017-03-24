5d45b3c8f4af6fce7415cd6a8e43d7db604dc292
coderconvoy <thebeaky@psmail.net>
2017-03-24 09:39:27 +0000

    ParseAmount fixed conversion bugs

--- history/htypes.go
+++ history/htypes.go
@@ -3,7 +3,6 @@ package history
 import (
 	"errors"
 	"fmt"
-	"strconv"
 	"strings"
 	"time"
 )
@@ -20,15 +19,47 @@ func NewACKey(s string) (ACKey, error) {
 	return ACKey{sp[0], sp[1]}, nil
 }
 
-func ParseAmount(s string) (int, error) {
-	am, err := strconv.ParseFloat(s, 64)
+func ParseAmount(st string) (int, error) {
+	dotted := -1
+	res := 0
+	for _, s := range st {
+		if dotted >= 0 {
+			dotted++
+			if dotted > 3 {
+				return 0, errors.New("dp too small")
+			}
+		}
+		switch s {
+		case '.':
+			if dotted >= 0 {
+				return 0, errors.New("Too many dots")
+			}
+			dotted = 1
+		case '0', '1', '2', '3', '4', '5', '6', '7', '8', '9':
+			n := int(s - '0')
+			res = res*10 + n
+		default:
+			return 0, errors.New("Unexpected Char: " + string(s))
+		}
+
+	}
+	if dotted == -1 {
+		return res * 100, nil
+	}
+	for dotted < 3 {
+		res *= 10
+		dotted++
+	}
+	return res, nil
+
+	/*am, err := strconv.ParseFloat(s, 64)
 	if err != nil {
 		return 0, errors.New("Could not parse amount")
 	}
 	if am < 0 {
 		return 0, errors.New("Amount not Positive")
 	}
-	return int(am * 100), nil
+	return int(am * 100), nil*/
 }
 
 func (a ACKey) String() string { return a.Username + ":" + a.Name }
--- history/split_test.go
+++ history/split_test.go
@@ -92,12 +92,17 @@ func Test_Amount(t *testing.T) {
 		{"helo", 0, true},
 		{"0.4", 40, false},
 		{"-1.7", 0, true},
+		{"1.54-", 0, true},
+		{"1.15", 115, false},
+		{"1.453", 0, true},
+		{"134", 13400, false},
+		{"135.3", 13530, false},
 	}
 
 	for _, ti := range tr {
 		o, err := ParseAmount(ti.In)
 		if o != ti.Out || ((err == nil) == ti.Err) {
-			t.Log("expected :%d,%b. Got %d,%s", ti.Out, ti.Err, o, err)
+			t.Logf("%s,expected :%d,%s. Got %d,%s", ti.In, ti.Out, ti.Err, o, err)
 			t.Fail()
 		}
 
