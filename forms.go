8416a815d3a530dd2dae8dec0a1a431210ada507
coderconvoy <thebeaky@psmail.net>
2017-04-27 14:46:07 +0100

    Added Form Pay to forms, and the account select boxes forms.go

--- forms.go
+++ forms.go
@@ -17,8 +17,35 @@ func HTMLColorPicker(id string, colors []string) *htmk.Tag {
 	return htmk.NewParent("select", ops, "name", id, "id", id)
 }
 
+func SelectMyAccounts(ld LoginData, tagname string) *htmk.Tag {
+	ops := []*htmk.Tag{}
+	for _, v := range ld.Fam.ListWriteAccess(ld.Fmem) {
+		s := v.Username + ":" + v.Name
+		ops = append(ops, htmk.NewTextTag("option", s+": "+PrintMoney(v.End), "value", s))
+	}
+	return htmk.NewParent("select", ops, "name", tagname)
+}
+
+func SelectAllAccounts(ld LoginData, tagname string) *htmk.Tag {
+	ops := []*htmk.Tag{}
+	for _, v := range ld.Fam.Period.Accounts {
+		s := v.Username + ":" + v.Name
+		ops = append(ops, htmk.NewTextTag("option", s, "value", s))
+	}
+	return htmk.NewParent("select", ops, "name", tagname)
+}
+
+func SelectAllUsers(ld LoginData, tagname string) *htmk.Tag {
+	ops := []*htmk.Tag{}
+	for _, v := range ld.Fam.Members {
+		ops = append(ops, htmk.NewTextTag("option", v.Username, "value", v.Username))
+	}
+	return htmk.NewParent("select", ops, "name", tagname)
+}
+
 func FormAddAccount(ld LoginData) *htmk.Tag {
 	return htmk.NewParent("form", []*htmk.Tag{
+		htmk.NewTextTag("h3", "Add Account"),
 		htmk.NewTag("input", "type", "text", "name", "username", "value", ld.Fmem, "--readonly", "--hidden"),
 		htmk.NewTag("input", "type", "text", "name", "accountname", "pattern", ".{4,20}", "--required"),
 		HTMLColorPicker("Col1", []string{}),
@@ -26,4 +53,16 @@ func FormAddAccount(ld LoginData) *htmk.Tag {
 		htmk.NewTextTag("div", "black:black", "class", "pocket", "id", "color_pocket"),
 		htmk.NewTag("input", "type", "submit", "value", "add"),
 	}, "id", "frm_add_account", "action", "addaccount", "method", "post")
+}
+
+func FormPay(ld LoginData) *htmk.Tag {
+	return htmk.NewParent("form", []*htmk.Tag{
+		htmk.NewTextTag("h3", "Pay Someone"),
+		htmk.NewTag("input", "type", "text", "name", "username", "value", ld.Fmem, "--readonly", "--hidden"),
+		htmk.NewText("<br>From:"), SelectMyAccounts(ld, "from"),
+		htmk.NewText("<br>To:"), SelectAllAccounts(ld, "to"),
+		htmk.NewText("<br>Amount:"), htmk.NewTag("input", "type", "number", "step", "0.01", "min", "0", "name", "amount"),
+		htmk.NewText("<br>Purpose:"), html.NewTag("input", "type", "text", "name", "purpose"),
+		html.NewTag("input", "type", "submit", "value", "Pay Now"),
+	}, "id", "frm_pay", "action", "pay", "method", "post")
 }
