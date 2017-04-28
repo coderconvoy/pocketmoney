e63441c65814d18c9771751751bc55a0d63e77d9
coderconvoy <thebeaky@psmail.net>
2017-04-28 11:49:50 +0100

    moving to using qfuncs from htmlmaker

--- forms.go
+++ forms.go
@@ -2,6 +2,8 @@
 package main
 
 import (
+	"time"
+
 	htmk "github.com/coderconvoy/htmlmaker"
 )
 
@@ -46,23 +48,38 @@ func SelectAllUsers(ld LoginData, tagname string) *htmk.Tag {
 func FormAddAccount(ld LoginData) *htmk.Tag {
 	return htmk.NewParent("form", []*htmk.Tag{
 		htmk.NewTextTag("h3", "Add Account"),
-		htmk.NewTag("input", "type", "text", "name", "username", "value", ld.Fmem, "--readonly", "--hidden"),
-		htmk.NewTag("input", "type", "text", "name", "accountname", "pattern", ".{4,20}", "--required"),
+		htmk.QInput("text", "username", "value", ld.Fmem, "--readonly", "--hidden"),
+		htmk.QInput("text", "accountname", "pattern", ".{4,20}", "--required"),
 		HTMLColorPicker("Col1", []string{}),
 		HTMLColorPicker("Col2", []string{}),
 		htmk.NewTextTag("div", "black:black", "class", "pocket", "id", "color_pocket"),
-		htmk.NewTag("input", "type", "submit", "value", "add"),
+		htmk.QSubmit("Add"),
 	}, "id", "frm_add_account", "action", "addaccount", "method", "post")
 }
 
 func FormPay(ld LoginData) *htmk.Tag {
 	return htmk.NewParent("form", []*htmk.Tag{
 		htmk.NewTextTag("h3", "Pay Someone"),
-		htmk.NewTag("input", "type", "text", "name", "username", "value", ld.Fmem, "--readonly", "--hidden"),
+		htmk.QInput("text", "username", "value", ld.Fmem, "--readonly", "--hidden"),
 		htmk.NewText("<br>From:"), SelectMyAccounts(ld, "from"),
 		htmk.NewText("<br>To:"), SelectAllAccounts(ld, "to"),
-		htmk.NewText("<br>Amount:"), htmk.NewTag("input", "type", "number", "step", "0.01", "min", "0", "name", "amount"),
-		htmk.NewText("<br>Purpose:"), html.NewTag("input", "type", "text", "name", "purpose"),
-		html.NewTag("input", "type", "submit", "value", "Pay Now"),
+		htmk.NewText("<br>Amount:"), htmk.QInput("number", "amount", "step", "0.01", "min", "0"),
+		htmk.NewText("<br>Purpose:"), html.QInput("text", "purpose"),
+		html.QSubmit("Pay Now"),
 	}, "id", "frm_pay", "action", "pay", "method", "post")
+}
+
+func FormStanding(ld LoginData) *htmk.Tag {
+	return htmk.NewParent("form", []*htmk.Tag{
+		htmk.NewTextTag("h3", "Make a Standing Order"),
+		htmk.NewText("<br>From:"), SelectMyAccounts(ld, "from"),
+		htmk.NewText("<br>To:"), SelectAllAccounts(ld, "to"),
+		htmk.NewText("<br>Amount: £"), htmk.NewTag("input", "type", "number", "step", "0.01", "min", "0", "name", "amount"),
+		htmk.NewText("<br>Purpose:"), htmk.NewTag("type", "text", "name", "purpose"),
+		htmk.NewText("<br>Start Date:"), htmk.NewTag("type", "date", "name", "start", "value", time.Now().Format("2006-01-02")),
+		htmk.NewText("<br>Then Every:"), htmk.NewTag("type", "number", "min", "1", "name", "delay", "value", "7"),
+		htmk.QSelect("delay_type", "days", "months"),
+		htmk.NewText("<br>"), htmk.QSubmit("Create"),
+	}, "id", "frm_standing", "action", "addstanding", "method", "post")
+
 }
